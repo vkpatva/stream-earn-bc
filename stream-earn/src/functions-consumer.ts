1f67aab56c5b73363b7ea627a532fc182183fe8c
Viraj Patva <viraj.patva@smartsensesolutions.com>
2023-11-29 12:38:49 +0530

    Initialize subgraph

---
+++ stream-earn/src/functions-consumer.ts
@@ -0,0 +1,103 @@
+import {
+  OwnershipTransferRequested as OwnershipTransferRequestedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  RequestFulfilled as RequestFulfilledEvent,
+  RequestSent as RequestSentEvent,
+  Response as ResponseEvent,
+  mintTokens as mintTokensEvent
+} from "../generated/FunctionsConsumer/FunctionsConsumer"
+import {
+  OwnershipTransferRequested,
+  OwnershipTransferred,
+  RequestFulfilled,
+  RequestSent,
+  Response,
+  mintTokens
+} from "../generated/schema"
+
+export function handleOwnershipTransferRequested(
+  event: OwnershipTransferRequestedEvent
+): void {
+  let entity = new OwnershipTransferRequested(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRequestFulfilled(event: RequestFulfilledEvent): void {
+  let entity = new RequestFulfilled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.FunctionsConsumer_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRequestSent(event: RequestSentEvent): void {
+  let entity = new RequestSent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.FunctionsConsumer_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleResponse(event: ResponseEvent): void {
+  let entity = new Response(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.requestId = event.params.requestId
+  entity.response = event.params.response
+  entity.err = event.params.err
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handlemintTokens(event: mintTokensEvent): void {
+  let entity = new mintTokens(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.requestId = event.params.requestId
+  entity.number = event.params.number
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
