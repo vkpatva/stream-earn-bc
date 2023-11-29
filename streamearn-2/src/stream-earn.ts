bde2168bfe12fb84801ef05fe0551b9c40ec1555
Viraj Patva <viraj.patva@smartsensesolutions.com>
2023-11-29 16:34:19 +0530

    Initialize subgraph

---
+++ streamearn-2/src/stream-earn.ts
@@ -0,0 +1,166 @@
+import {
+  ArtistRemoved as ArtistRemovedEvent,
+  ArtistWhitelisted as ArtistWhitelistedEvent,
+  DataRequestMade as DataRequestMadeEvent,
+  DataResponse as DataResponseEvent,
+  MintStreamTokens as MintStreamTokensEvent,
+  OwnershipTransferRequested as OwnershipTransferRequestedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  RequestFulfilled as RequestFulfilledEvent,
+  RequestSent as RequestSentEvent,
+  SongAdded as SongAddedEvent
+} from "../generated/StreamEarn/StreamEarn"
+import {
+  ArtistRemoved,
+  ArtistWhitelisted,
+  DataRequestMade,
+  DataResponse,
+  MintStreamTokens,
+  OwnershipTransferRequested,
+  OwnershipTransferred,
+  RequestFulfilled,
+  RequestSent,
+  SongAdded
+} from "../generated/schema"
+
+export function handleArtistRemoved(event: ArtistRemovedEvent): void {
+  let entity = new ArtistRemoved(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.artist = event.params.artist
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleArtistWhitelisted(event: ArtistWhitelistedEvent): void {
+  let entity = new ArtistWhitelisted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.artist = event.params.artist
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDataRequestMade(event: DataRequestMadeEvent): void {
+  let entity = new DataRequestMade(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.requestId = event.params.requestId
+  entity.songId = event.params.songId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDataResponse(event: DataResponseEvent): void {
+  let entity = new DataResponse(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.requestId = event.params.requestId
+  entity.response = event.params.response
+  entity.err = event.params.err
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMintStreamTokens(event: MintStreamTokensEvent): void {
+  let entity = new MintStreamTokens(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.artist = event.params.artist
+  entity.tokensMinted = event.params.tokensMinted
+  entity.songId = event.params.songId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferRequested(
+  event: OwnershipTransferRequestedEvent
+): void {
+  let entity = new OwnershipTransferRequested(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRequestFulfilled(event: RequestFulfilledEvent): void {
+  let entity = new RequestFulfilled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.StreamEarn_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleRequestSent(event: RequestSentEvent): void {
+  let entity = new RequestSent(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.StreamEarn_id = event.params.id
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleSongAdded(event: SongAddedEvent): void {
+  let entity = new SongAdded(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.artist = event.params.artist
+  entity.songId = event.params.songId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
